33723f6d2ebeb7f4c2531eaec995fbab4662720d
MyPlayground <57849991+MyPlayground@users.noreply.github.com>
2019-11-17 13:01:10 +0900

    LazyModule を追加
    モジュールの遅延読み込みの実装サンプル

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,10 @@ const routes: Routes = [
     component: HomeComponent
   },
   {
+    path: 'lazy',
+    loadChildren: './lazy/lazy.module#LazyModule'
+  },
+  {
     path: '**',
     redirectTo: ''
   }
