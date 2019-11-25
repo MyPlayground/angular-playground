01d6a714a2d049547218d675df57bb8b3fe36917
MyPlayground <57849991+MyPlayground@users.noreply.github.com>
2019-11-26 00:30:49 +0900

    ページ用の Store を作成

---
+++ src/app/sandbox/pages/city-search-page/store/city-search-page-store.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import * as fromCitySearchPage from './';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StoreModule.forFeature(fromCitySearchPage.citySearchPageFeatureKey, fromCitySearchPage.reducers, { metaReducers: fromCitySearchPage.metaReducers })
+  ]
+})
+export class CitySearchPageStoreModule { }
