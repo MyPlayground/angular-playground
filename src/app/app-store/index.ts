891172447a8346016ae09fab4bed759eb3f89cad
MyPlayground <57849991+MyPlayground@users.noreply.github.com>
2019-11-26 00:19:49 +0900

    ログ出力の設定とRedux DevTools Extensionを使えるようにする

--- src/app/app-store/index.ts
+++ src/app/app-store/index.ts
@@ -1,20 +1,19 @@
-import {
-  ActionReducer,
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-  MetaReducer
-} from '@ngrx/store';
+import { ActionReducer, ActionReducerMap, createFeatureSelector, createSelector, MetaReducer } from '@ngrx/store';
 import { environment } from '../../environments/environment';
 
+export interface State {}
 
-export interface State {
+export const reducers: ActionReducerMap<State> = {};
 
+export function logger(reducer: ActionReducer<State>): ActionReducer<State> {
+  return (state, action) => {
+    const result = reducer(state, action);
+    console.groupCollapsed(action.type);
+    console.log('action', action);
+    console.log('state', result);
+    console.groupEnd();
+    return result;
+  };
 }
-
-export const reducers: ActionReducerMap<State> = {
-
-};
-
 
 export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
